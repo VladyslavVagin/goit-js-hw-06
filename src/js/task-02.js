f9677adc8efbcb2ba987bf02e945ceee0e45610f
VladyslavVagin <140642012+VladyslavVagin@users.noreply.github.com>
2023-10-01 00:38:08 +0200

    Task-02

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+// Choose element from HTML document (element 'ul')
+const mainList = document.querySelector('#ingredients');
+
+// Make function for creation list items 
+const createListItems = function (ingredients) {
+  return ingredients.map(ingredient => {
+    const createItem = document.createElement('li');
+    createItem.textContent = `${ingredient}`;
+    createItem.classList = 'item';
+    return createItem;
+  })
+}
+// add new items to html document (using spread)
+mainList.append(...createListItems(ingredients));
+
+console.log(mainList);
