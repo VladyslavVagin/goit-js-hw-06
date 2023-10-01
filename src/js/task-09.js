0e4c243422f51609b336b451917653b6681641c6
VladyslavVagin <140642012+VladyslavVagin@users.noreply.github.com>
2023-10-01 16:09:27 +0200

    Task-09

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,10 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const btnChangeColor = document.querySelector('.change-color');
+const spanShowColor = document.querySelector('.color');
+const bodyShow = document.querySelector('body');
+
+btnChangeColor.addEventListener('click',
+  () => bodyShow.style.backgroundColor = spanShowColor.textContent = getRandomHexColor());
+// ============================================================================
