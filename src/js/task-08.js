f06c833afd73de8a1475ba5a7ef9620951b59fd8
VladyslavVagin <140642012+VladyslavVagin@users.noreply.github.com>
2023-10-01 15:52:30 +0200

    Task-08

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,25 @@
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', onSubmitForm);
+
+function onSubmitForm(event) {
+    event.preventDefault();
+
+    const formElements = event.currentTarget.elements;
+    const email = formElements.email.value;
+    const password = formElements.password.value;
+
+    if (email === '' || password === '') {
+        return alert('ALL INPUTS SHOULD BE FILLED!!!!!');
+    }
+    const formData = {
+        email,
+        password,
+    };
+    console.log(formData);
+    event.currentTarget.reset();
+};
+
+
+
 
