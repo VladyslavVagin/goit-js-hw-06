4d8a92e99fba8dae56274ce48c851ad9ed934ce7
VladyslavVagin <140642012+VladyslavVagin@users.noreply.github.com>
2023-10-01 20:56:31 +0200

    Task-05(corrections)
    add checks (if "" or " ") will write "Anonimous"

--- js/task-05.js
+++ js/task-05.js
@@ -2,7 +2,9 @@ const ref = {
     span: document.querySelector('#name-output'),
     input: document.querySelector('#name-input'),
 }
-ref.input.addEventListener('input', event => ref.span.textContent = event.currentTarget.value);
+ref.input.addEventListener('input', event => 
+event.currentTarget.value === '' || event.currentTarget.value === ' ' ? ref.span.textContent = 'Anonimous' :
+ref.span.textContent = event.currentTarget.value);
 
 
 
