1c60fa91e9da6beab2230cd32b470e19cf07ceaa
VladyslavVagin <140642012+VladyslavVagin@users.noreply.github.com>
2023-10-01 17:27:27 +0200

    Task-10

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -3,3 +3,31 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const createButton = document.querySelector('[data-create]');
+const destroyButton = document.querySelector('[data-destroy]');
+const inputNumber = document.querySelector('input');
+const container = document.querySelector('div#boxes');
+
+let amount = 0;
+
+function createBoxes(amount) {
+  let param = 20;
+  for (let i = 0; i < amount; i += 1) {
+    param += 10;
+    const boxItem = `<div style='background-color: ${getRandomHexColor()}; width: ${param}px; height: ${param}px'></div>`;
+    container.insertAdjacentHTML("afterbegin", boxItem);
+  }
+}
+
+createButton.addEventListener('click', onClickAddInput);
+destroyButton.addEventListener('click', onClickClearAll);
+
+function onClickAddInput(amount) {
+  amount = inputNumber.value;
+  createBoxes(amount);
+}
+function onClickClearAll() {
+  inputNumber.value = "";
+  container.innerHTML = "";
+}
